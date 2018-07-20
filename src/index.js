f14d468bc4dd35d33f70a688d1272f224a580bd5
pjehan <pierre.jehan@gmail.com>
2018-07-20 23:00:09 +0200

    Add refresh and change movies on refresh

--- src/index.js
+++ src/index.js
@@ -5,20 +5,25 @@ class MovieDB extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {movies: []};
+        this.state = {movies: [], counter: 0};
     }
 
     componentDidMount() {
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=f12fad38445dcbe07205ca2571cbdd14&language=fr-FR&page=1')
+        const { ApiKey } = this.props;
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}&language=fr-FR&page=1`)
             .then(response => response.json())
             .then(data => {
-                    const results = data.results;
-                    this.setState({movies: results});
+                    this.setState({movies: data.results});
+                    this.timer = setInterval(() => {
+                        const { movies, counter } = this.state;
+                        this.props.animate().then(() => this.setState({counter: ((counter + 1) * 3) >= movies.length ? 0 : counter + 1}));
+                    }, this.props.refreshInterval || 5000);
                 }
             );
     }
 
     componentWillUnmount() {
+        clearInterval(this.timer);
     }
 
     render() {
@@ -30,25 +35,22 @@ class MovieDB extends Component {
             height: '100%',
             backgroundColor: 'pink'
         };
-        const {movies} = this.state;
-        const movieTop3 = [];
-        movies.forEach((element, index) => {
-            if (index <= 2) {
-                movieTop3.push(element);
-            }
-        });
+
+        const { movies, counter } = this.state;
+        const offset = 3 * counter;
         const urlSrcImage = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
+
+        const items = movies.slice(offset, offset + 3).map(movie => (
+          <div key={movie.id} className="contentMovie">
+            <h4 className="titleMovie">{movie.title}</h4>
+            <p>Notes: {movie.vote_average}</p>
+            <img src={urlSrcImage + movie.poster_path} max-width="50px" className="poster-movie"/>
+          </div>
+        ));
+
         return (
             <div className="containerMovie">
-                {
-                    movieTop3.map(movie =>
-                    <div key={movie.id} className="contentMovie">
-                        <h4 className="titleMovie">{movie.title}</h4>
-                        <p>Notes: {movie.vote_average}</p>
-                        <img src={urlSrcImage + movie.poster_path} max-width="50px" className="poster-movie"/>
-                    </div>
-                )
-                }
+                {items}
             </div>
         );
     }
